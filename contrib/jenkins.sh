0f760a64769c63e267532080f476f63a42eda339
Neels Hofmeyr <neels@hofmeyr.de>
2017-10-27 22:54:05 +0200

    jenkins: use osmo-clean-workspace.sh before and after build
    See osmo-ci change I2409b2928b4d7ebbd6c005097d4ad7337307dd93 for rationale.
    
    Depends: I2409b2928b4d7ebbd6c005097d4ad7337307dd93
    Change-Id: I0030c8662fa87fb589582a3f6bde6ed53e8e3ef9

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -2,8 +2,12 @@
 
 set -ex
 
+osmo-clean-workspace.sh
+
 autoreconf --install --force
 ./configure
 $MAKE CFLAGS="-Werror" CPPFLAGS="-Werror"
 # currently broken $MAKE $PARALLEL_MAKE
 # currently broken $MAKE distcheck
+
+osmo-clean-workspace.sh
