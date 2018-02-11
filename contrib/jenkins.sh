882a759818f2c274e3ff6370e40d2e4621fd6ce0
Martin Hauke <mardnh@gmx.de>
2018-02-11 10:27:13 +0100

    jenkins: Enable parallel build and distcheck
    Change-Id: Id41fbcb5a96093eb6c3dc00bcacbd379111ada70

--- contrib/jenkins.sh
+++ contrib/jenkins.sh
@@ -7,7 +7,7 @@ osmo-clean-workspace.sh
 autoreconf --install --force
 ./configure --enable-sanitize
 $MAKE CFLAGS="-Werror" CPPFLAGS="-Werror"
-# currently broken $MAKE $PARALLEL_MAKE
-# currently broken $MAKE distcheck
+$MAKE $PARALLEL_MAKE
+$MAKE distcheck
 
 osmo-clean-workspace.sh
